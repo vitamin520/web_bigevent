c21f00ad1add99b22e944b2b6dc3149916c75109
Your Name <you@example.com>
2022-06-23 11:15:10 +0800

    完成了主页功能的开发

--- assets/js/index.js
+++ assets/js/index.js
@@ -25,14 +25,18 @@ function getUserInfo() {
   $.ajax({
     method: 'GET',
     url: '/my/userinfo',
+    // headers就是请求配置对象(此部分统一写在了baseAPI中)
+    // headers: {
+    //   Authorization: localStorage.getItem('token') || ''
+    // },
     success: function(res) {
       if (res.status !== 0) {
         return layui.layer.msg('获取用户信息失败！')
       }
       // 调用 renderAvatar 渲染用户的头像
       renderAvatar(res.data)
-    }
-    // 不论成功还是失败，最终都会调用 complete 回调函数
+    },
+    // 不论成功还是失败，最终都会调用 complete 回调函数(此部分统一写在了baseAPI中)
     // complete: function(res) {
     //   // console.log('执行了 complete 回调：')
     //   // console.log(res)
